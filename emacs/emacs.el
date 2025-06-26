9a4b5d1e7de8fd9cdb0b35ab87aa071e11fcb1ef
Mark Broeders <mail@markbroeders.nl>
2025-06-26 16:30:36 +0200

    Various performance improvements

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -98,6 +98,14 @@
                          (emacs-init-time)
                          (number-to-string (length package-activated-list))))))))
 
+;; Adjust gc-cons-threshold
+;; The default setting is too low for lsp-mode's needs due to the fact that client/server communication generates a lot of memory/garbage.
+(setq gc-cons-threshold 100000000)
+
+;; Increase the amount of data which Emacs reads from the process
+;; Again the emacs default is too low 4k considering that the some of the language server responses are in 800k - 3M range.
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+
 (use-package all-the-icons
   :ensure t
   :if (display-graphic-p))
